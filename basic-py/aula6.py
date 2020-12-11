694d8e4d76b459344761f5040ea2b1bc84beeef2
kaiquepill <kaique.gomes@bandtec.com.br>
2020-12-11 20:52:01 -0300

    if, else

---
+++ basic-py/aula6.py
@@ -0,0 +1,12 @@
+"""
+IF, ELIF E ELSE
+"""
+
+
+nome = input()
+if 'kaique'.__eq__(nome):
+    print("verdadeiro.")
+elif nome.startswith('c'):
+    print("verdadeiro pt2.")
+else:
+    print("falso.")
