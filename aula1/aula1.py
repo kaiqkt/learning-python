178503c42c7324d86020a248a7480583c6db34cf
kaiquepill <kaique.gomes@bandtec.com.br>
2020-12-11 19:26:09 -0300

    comentarios e print

---
+++ aula1/aula1.py
@@ -0,0 +1,19 @@
+print('Linha 1')
+print('Linha 2')
+# Falando sobre do que se trata essa linha
+print('Linha 3')
+print('Linha 4')
+print('Linha 5')
+print('Linha 6')
+'''
+Comentario
+'''
+
+print('Linha 6', 'Kaique')
+print('overseas', 'service', sep='-')
+
+"""
+151.151.151-15
+"""
+print('151', '151', '151', sep='.', end='-')
+print('15')
