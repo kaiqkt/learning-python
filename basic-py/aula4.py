f2f89200840353493d33d3cc3337b202773d76b9
kaiquepill <kaique.gomes@bandtec.com.br>
2020-12-11 20:15:48 -0300

    formatar str

--- basic-py/aula4.py
+++ basic-py/aula4.py
@@ -10,3 +10,12 @@ data_atual = 2020
 imc = peso / (altura ** 2)
 
 print(nome, 'tem', idade, 'anos de idade e seu imc é', imc)
+
+"""
+Formatacao de sting
+"""
+
+print(f'{nome} tem {idade} anos de idade e seu imc é {imc:.2f}')  # :.2f arrendondar numeros decimais
+print('{} tem {} anos de idade e seu imc é {:.2f}'.format(nome, idade, imc))
+print('{0} tem {2} anos de idade e seu imc é {1:.2f}'.format(nome, idade, imc))  # define a ordem das variaveis
+print('{n} tem {p} anos de idade e seu imc é {q:.2f}'.format(n=nome, p=idade, q=imc))  # nomeia as variaveis
