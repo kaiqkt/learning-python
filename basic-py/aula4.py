89325476c45e729cbc6453bd3df5cb67831d9cbd
kaiquepill <kaique.gomes@bandtec.com.br>
2020-12-11 20:02:37 -0300

    variaves

---
+++ basic-py/aula4.py
@@ -0,0 +1,12 @@
+"""
+Variaveis
+"""
+
+nome = 'Kaique'
+idade = 18
+altura = 1.76
+peso = 60
+data_atual = 2020
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', idade, 'anos de idade e seu imc é', imc)
