a00fdaa9904016627626a2352699a141f4620bff
kaiquepill <kaique.gomes@bandtec.com.br>
2020-12-11 20:26:50 -0300

    input

---
+++ basic-py/aula5.py
@@ -0,0 +1,11 @@
+"""
+entrada de dados
+"""
+
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade ")
+ano_n = 2020 - int(idade)
+print(f'o usuario digitou {nome} e o tipo da varivael é {type(nome)}')
+print(f'seu nome é {nome} e nascem em {ano_n}')
+
+
