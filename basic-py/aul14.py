7cf9ba515e9b13c2eb2cb818252335e17bda4c7b
kaiquepill <kaique.gomes@bandtec.com.br>
2020-12-12 14:43:56 -0300

    while/else

---
+++ basic-py/aul14.py
@@ -0,0 +1,20 @@
+"""
+While / Else
+contadores
+acumuladores
+"""
+
+contador = 1
+acumulador = 1
+
+while contador <= 10:
+    print(contador, acumulador)
+
+    if contador > 5:
+        break
+
+    acumulador = acumulador + contador
+    contador += 1
+else:
+    print('cabo')
+print('cabo')
