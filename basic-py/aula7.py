22bdec1102d1ceb41197231372aed293caadbbe8
kaiquepill <kaique.gomes@bandtec.com.br>
2020-12-11 21:28:25 -0300

    operador logico

---
+++ basic-py/aula7.py
@@ -0,0 +1,20 @@
+"""
+Operadores logicos
+and, or, not
+in e not in
+"""
+
+if 2 == 2 and 3 == 3:
+    print("a")
+
+if 2 == 2 or 4 == 3:
+    print("b")
+
+if not 3 == 2:
+    print("c")
+
+if 'u' in 'kaique':
+    print('ok')
+
+if 'u' not in 'kaique':
+    print('ok')
