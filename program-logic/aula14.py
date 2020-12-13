02af42122ac42d34b3bf06b4cf70b487ce50d1fd
kaiquepill <kaique.gomes@bandtec.com.br>
2020-12-13 02:13:52 -0300

    gerador de cpf

--- program-logic/aula14.py
+++ program-logic/aula14.py
@@ -1,22 +1,20 @@
 """
-iterando string com while
+While / Else
+contadores
+acumuladores
 """
 
-frase = 'o rato roeu a roupa do rei de roma'
-frase_len = len(frase)
-print(frase_len)
-x = 0
-nova_string = ''
+contador = 1
+acumulador = 1
 
-input_user = input("Qual?: ")
+while contador <= 10:
+    print(contador, acumulador)
 
-while x < frase_len:
-    # print(frase[x], x)
-    letra = frase[x]
-    if letra.__eq__(input_user):
-        nova_string += letra.upper()
-    else:
-        nova_string += frase[x]
-    print(nova_string)
-    x += 1
-print(nova_string)
+    if contador > 5:
+        break
+
+    acumulador = acumulador + contador
+    contador += 1
+else:
+    print('cabo')
+print('cabo')
