1b8d7c1d48c4a8fd83919fcab452b9997ba75c6c
kaiquepill <kaique.gomes@bandtec.com.br>
2020-12-20 18:30:59 -0300

    try e except como condicional

--- procedural-programming/aula18.py
+++ procedural-programming/aula18.py
@@ -10,3 +10,24 @@ try:
     print(divide(2, 0))
 except ZeroDivisionError as error:
     print(error)
+
+
+def converte_numero(v):
+    try:
+        v = int(v)
+        return v
+    except ValueError:
+        try:
+            v = float(v)
+            return v
+        except ValueError as error:
+            print(error)
+            pass
+
+
+numero = converte_numero(input('Digute um numero: '))
+
+if numero is not None:
+    print(numero * 3)
+else:
+    print('Isso nao é um numero')
