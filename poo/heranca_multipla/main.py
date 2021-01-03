53642fdcd603c17a3bb8f91a6da076c3a1988878
kaiquepill <kaique.gomes@bandtec.com.br>
2021-01-03 06:55:22 -0300

    Heranca multipla

---
+++ poo/heranca_multipla/main.py
@@ -0,0 +1,15 @@
+from smartphone import Smartphone
+
+smartphone = Smartphone('Pocophone F1')
+smartphone.ligar()
+smartphone.conetar()
+smartphone.conetar()
+# smartphone.desligar()
+# smartphone.ligar()
+# smartphone.conetar()
+# smartphone.conetar()
+# smartphone.conetar()
+# smartphone.desligar()
+# smartphone.conetar()
+# smartphone.desconectar()
+# smartphone.desconectar()
